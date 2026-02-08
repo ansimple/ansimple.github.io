af42d458b8cb2ea79381bd357db9a7f788df0aad
SIMPLE-BOOK-PRO\ansim <ansimple2000@naver.com>
2026-02-08 20:59:43 +0900

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/phonics_pop_pop/sw.js', { scope: '/phonics_pop_pop/' })})}
